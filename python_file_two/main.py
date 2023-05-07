8644397957075efbbe8cdac7d73df7ec57639f49
Dmytro Romanenko <iamcreator13@gmail.com>
2023-05-07 13:53:12 +0300

    Wrote simple calculator for cookies

--- python_file_two/main.py
+++ python_file_two/main.py
@@ -1,1 +1,10 @@
-print('Hello World!')
+print('Hey, how much money do your have?')
+amount = float(input('I have = '))
+cookie = float(input('Please, enter price for cookie here = '))
+candy = float(input('Please, enter price for candy here = '))
+
+totalPrice = cookie + candy
+if amount >= totalPrice:
+    print('Hey, u have enough money')
+else:
+    print('Hey, not enough money in here!')
