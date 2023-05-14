edaa10a11d4660c613a4b3b971205d598e46a449
Dmytro Romanenko <iamcreator13@gmail.com>
2023-05-14 17:54:36 +0300

    add description to code

---
+++ First_homework_with_tasks/income_task.py
@@ -0,0 +1,6 @@
+income = float(input('Введите сумму вашего дохода: '))
+precent_from_salary = float(input('Введите процент, который вы платите со своего дозода: '))
+
+should_pay = income / 100 * precent_from_salary
+print(should_pay)
+
